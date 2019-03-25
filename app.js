2de294e2cd80d8b513ed199bd7032793e8daee73
SergeiSko <34917913+SergeiSko@users.noreply.github.com>
2019-03-25 22:54:09 +0300

    25.03.19 last

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-void express = require('express');
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+  res.send("hello world");
+})
+
+app.get("*", function(req, res){
+  res.send('not found');
+});
+
+app.listen(port, function(){
+  console.log(`Listening: ${port} port.`);
+});
