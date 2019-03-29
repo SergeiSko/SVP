0c44daff02aa70f3a1fafc975641807a9cd6d99d
SergeiSko <34917913+SergeiSko@users.noreply.github.com>
2019-03-29 22:33:17 +0300

    test-ы post запросов

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var express = require('express');
-var app = express();
-var port = 3000;
+    parser = require('body-parser');
+    app = express();
+    port = 3000;
+//var file = require('test.json');
 
 app.use(express.static(__dirname + '/public'));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain')
-  res.send("hello world");
-})
+  res.send('public/index.html');
+});
 
+app.get("/getJSON", function(req, res){
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.sendfile('test.json');
+});
 
-
-
+app.post("/postDate", function(req, res){
+  console.log(req.body.name);
+  res.send(`Идите нахуй ${req.body.name}`);
+});
 
 
 
