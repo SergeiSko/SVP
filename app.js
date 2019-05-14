57c6f46405e4655f4c7d55760cd022b37c4f257b
SergeiSko <34917913+SergeiSko@users.noreply.github.com>
2019-05-14 19:02:17 +0300

    add mongo

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-var express = require('express');
-    parser = require('body-parser');
-    mongodb = require('mongodb').MongoClient;
-    app = express();
-    port = 3000;
+const express = require('express');
+      parser = require('body-parser');
+      MongoClient = require('mongodb').MongoClient;
+      ejs = require('ejs');
+      promisify = require('util').promisify;
+      app = express();
+      port = 3000;
 //var file = require('test.json');
 
 app.use(express.static(__dirname + '/public'));
 app.use(parser.json());
 app.use(parser.urlencoded({extended: false}));
 
+const mongoClient = new MongoClient("mongodb://localhost:27017", {useNewUrlParser: true});
+mongoClient.connect(function(err, client) {
+  if(err){
+    return console.log(err);
+  }
+
+  const db = client.db("dbV1");
+  const collection = db.collection("users");
+
+
+  client.close();
+});
+
+const requestHandler = (req, res) => {
+  const renderFile = promisify(ejs.renderFile).bind(ejs);
+
+  async function renderTemplare(){
+    try{
+      return await renderFile(__dirname + '/templates/template.ejs');
+    } catch(err){
+      throw(err);
+    }
+  }
+}
+
 app.get("/", function(req, res){
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('public/index.html');
+  res.setHeader('Content-Type', 'text/plain');
+  
+  res.send(__dirname + 'public/index.html');
 });
 
 app.get("/getJSON", function(req, res){
@@ -45,7 +73,7 @@ app.post("/test", function(req, res){
   if(res != "undefined"){
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.send("god")
+    res.send('god');
   }
 });
 
