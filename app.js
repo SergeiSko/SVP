03a1467f1f3450d294c4def31cc3bacf3ed19ce1
SergeiSko <34917913+SergeiSko@users.noreply.github.com>
2019-05-18 18:58:42 +0300

    CleanUpBlond

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require('express');
       MongoClient = require('mongodb').MongoClient;
       app = express();
       port = 3000;
-      ejs = require('ejs');
-      promisify = require('util').promisify;
-      users = require(__dirname + '\\public\\libs\\users');
-//var file = require('test.json');
 
 app.use("public", express.static(__dirname + '/public'));
 app.use(parser.json());
@@ -25,29 +21,10 @@ mongoClient.connect(function(err, client) {
   client.close();
 });
 
-const requestHandler = (req, res) => {
-  const renderFile = promisify(ejs.renderFile).bind(ejs);
-
-  async function renderTemplare(){
-    try{
-      return await renderFile(__dirname + '/templates/template.ejs');
-    } catch(err){
-      throw(err);
-    }
-  }
-}
-
 app.get("/", function(req, res){
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  //res.send('hhuuh');
-  //res.sendfile(__dirname + '\\public\\template\\template.ejs');
   //res.send(__dirname + 'public/index.html');
-  let params = {};
-  params.page = 'insert';
-
-  const wordArray = users.itemsRu;
-  res.send("-->" + users);
 });
 
 app.get("/getJSON", function(req, res){
