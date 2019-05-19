028c1a1aeee6d25620a105f45fc5c46e6c92aa15
SergeiSko <34917913+SergeiSko@users.noreply.github.com>
2019-05-19 20:16:10 +0300

    changeHTML

--- app.js
+++ app.js
@@ -41,12 +41,6 @@ app.get("/getJSON", function(req, res){
   res.sendfile('test.json');
 });
 
-app.get("/str", function(req, res){
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.send("Good response.");
-});
-
 app.post("/Data", function(req, res){
   console.log(req.body.name);
   if(res != "undefined"){
